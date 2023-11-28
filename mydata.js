db308ab8b29c253c1ff0be8cf93f96c94b426e31
Osom-Emmanuel <140416191+Osom-Emmanuel@users.noreply.github.com>
2023-11-28 07:36:43 -0800

    i run the map function here
    i run the map function here from the js file and not from the html file with a type of modules

---
+++ mydata.js
@@ -0,0 +1,25 @@
+export let services = [
+{
+    image:'./Academy.png',
+    title: 'Academy',
+    desc: 'Gain access to exclusive expert-ised courses that are student and hand-on driven\
+           master your craft and stand out globally with our master of practice certification.',
+    button: true
+},
+{
+    image: './ICT-Solutions.png',
+    title: ' ICT SOLUTIONS',
+    desc: 'Find out and bergain for best of the outstanding services we offer.\
+           We have an excellent network of professionals in team to meet your needs.',
+},
+{
+    image: './Marketplace.png',
+    title: 'Market place',
+    desc: 'Shop for quality and reliable innovative products, ideas, project resources, gadgets, and educational materials.',
+},
+{
+    image: './staff-product.png',
+    title: 'Staff Product',
+    desc: 'See outstanding portfolio of our graduates for your hire and partnership as the case may be.',
+}
+];
